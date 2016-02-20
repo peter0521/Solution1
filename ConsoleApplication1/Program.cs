e3f44b9c13526a6aceb300bc65ab3c01d22574eb
peter0521 <peter0521@gmail.com>
2016-02-20 11:58:38 +0800

    新增Class test2

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,26 @@
+﻿using SharedProject1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            test t = new test();
+            t.Id = 1;
+            t.Name = "peter";
+            Console.WriteLine(t.Id);
+            Console.WriteLine(t.Name);
+
+            test2 t2 = new test2();
+            t2.work = "design";
+            Console.WriteLine(t2.work);
+            
+        }
+    }
+}
---
+++ SharedProject1/test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SharedProject1
+{
+    class test
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    class test2
+    {
+        public int Id { get; set; }
+        public string work { get; set; }
+    }
+}
